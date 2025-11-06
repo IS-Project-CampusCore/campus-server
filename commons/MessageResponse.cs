5ec6b3bc85704e056eeef208e5d91acec203b81c
birrlea11 <birlea24@gmail.com>
2025-11-07 00:08:46 +0200

    finished messagebody task

--- commons/MessageResponse.cs
+++ commons/MessageResponse.cs
@@ -35,8 +35,67 @@ public readonly struct MessageBody
     public readonly int? TryInt32() => Validate(JsonValueKind.Number) ? _json.GetInt32() : null;
     public readonly JsonElement? TryObj() => Validate(JsonValueKind.Object) ? _json : null;
     public readonly JsonElement.ArrayEnumerator? TryArray() => Validate(JsonValueKind.Array) ? _json.EnumerateArray() : null;
+    public readonly string? TryGetString(string property) =>
+        TryGetProperty(property, JsonValueKind.String, out var prop) ? prop.GetString() : null;
+
+    public readonly int? TryGetInt32(string property) =>
+        TryGetProperty(property, JsonValueKind.Number, out var prop) ? prop.GetInt32() : null;
+
+    public readonly JsonElement? TryGetObject(string property) =>
+        TryGetProperty(property, JsonValueKind.Object, out var prop) ? prop : null;
+
+    public readonly JsonElement.ArrayEnumerator? TryGetArray(string property) =>
+        TryGetProperty(property, JsonValueKind.Array, out var prop) ? prop.EnumerateArray() : null;
+
+    public readonly string GetString(string property) =>
+        GetPropertyAndValidate(property, JsonValueKind.String).GetString()!;
+
+    public readonly int GetInt32(string property) =>
+        GetPropertyAndValidate(property, JsonValueKind.Number).GetInt32();
+
+    public readonly JsonElement GetObject(string property) =>
+        GetPropertyAndValidate(property, JsonValueKind.Object);
+
+    public readonly JsonElement.ArrayEnumerator GetArray(string property) =>
+        GetPropertyAndValidate(property, JsonValueKind.Array).EnumerateArray();
+
+    private readonly JsonElement GetPropertyAndValidate(string propertyName, JsonValueKind expectedKind)
+    {
+        ValidateJsonOrThrow(JsonValueKind.Object);
+
+        if (!_json.TryGetProperty(propertyName, out var propertyElement))
+        {
+            throw new ArgumentException($"Property '{propertyName}' not found on message body object."); // Or future MessageBodyException
+        }
+
+        if (propertyElement.ValueKind != expectedKind)
+        {
+            throw new ArgumentException($"Property '{propertyName}' is not a valid {expectedKind}."); // Or future MessageBodyException
+        }
+
+        return propertyElement;
+    }
+
+    private readonly bool TryGetProperty(string propertyName, out JsonElement element)
+    {
+        if (_json.ValueKind == JsonValueKind.Object)
+        {
+            return _json.TryGetProperty(propertyName, out element);
+        }
+        element = default;
+        return false;
+    }
+
+    private readonly bool TryGetProperty(string propertyName, JsonValueKind expectedKind, out JsonElement element) =>
+        TryGetProperty(propertyName, out element) && ValidateProperty(element, expectedKind);
+
+
+    private readonly bool ValidateProperty(JsonElement element, JsonValueKind expectedKind) =>
+        element.ValueKind == expectedKind;
+
 
     private bool Validate(JsonValueKind kind) => _json.ValueKind == kind;
+
     private JsonElement ValidateJsonOrThrow(JsonValueKind valueKind)
     {
         if (!Validate(valueKind))
