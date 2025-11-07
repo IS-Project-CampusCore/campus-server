a164f17cdc0cac49c58b3c5502655cfb14ec6a40
Doru <doru.boca@cityscroller.com>
2025-11-07 02:11:56 +0200

    Added Serilog to Docker Compose

--- SomeService/Program.cs
+++ SomeService/Program.cs
@@ -1,11 +1,24 @@
 using Grpc.Net;
 using Grpc.Net.Client;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Serilog;
 using SomeService;
 using SomeService.Services;
 using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var seqUrl = builder.Configuration["Logging:SeqUrl"];
+
+builder.Host.UseSerilog((context, config) =>
+{
+    config
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.Console()
+        .WriteTo.Seq(seqUrl!);
+});
 
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
