2fcb06ed6ece00d7e3f5b7aac522eba871d21bcf
birrlea11 <73988124+birrlea11@users.noreply.github.com>
2025-11-09 20:14:59 +0200

    Initial MessageBody (#3)

--- commons/MessageResponse.cs
+++ commons/MessageResponse.cs
@@ -96,12 +96,85 @@ public readonly struct MessageBody
     public readonly JsonElement Json => _json;
 
     public MessageBody() => _json = s_nullJson.RootElement;
+    
     public MessageBody(JsonElement json) => _json = json;
 
     public readonly string String() => ValidateJsonOrThrow(JsonValueKind.String).ToString();
+    
+    public readonly int Int32() => ValidateJsonOrThrow(JsonValueKind.Number).GetInt32();
+
+    public readonly MessageBody Object() => new(ValidateJsonOrThrow(JsonValueKind.Object));
+
+    public readonly JsonElement Array() => ValidateJsonOrThrow(JsonValueKind.Array);
+
+    public readonly string? TryString() => Validate(JsonValueKind.String) ? _json.GetString() : null;
+   
+    public readonly int? TryInt32() => Validate(JsonValueKind.Number) ? _json.GetInt32() : null;
+
+    public readonly MessageBody? TryObj() => Validate(JsonValueKind.Object) ? new(_json) : null;
+
+    public readonly JsonElement? TryArray() => Validate(JsonValueKind.Array) ? _json : null;
+
+    public readonly string? TryGetString(string property) =>
+        TryGetProperty(property, JsonValueKind.String) is { } prop ? prop.GetString() : null;
+
+    public readonly int? TryGetInt32(string property) =>
+        TryGetProperty(property, JsonValueKind.Number) is { } prop ? prop.GetInt32() : null;
+
+    public readonly MessageBody? TryGetObject(string property) => 
+        TryGetProperty(property, JsonValueKind.Object) is { } prop ? new(prop) : null;
+
+    public readonly JsonElement? TryGetArray(string property) => 
+        TryGetProperty(property, JsonValueKind.Array) is { } prop ? prop : null;
+
+    public readonly string GetString(string property) =>
+        GetPropertyAndValidate(property, JsonValueKind.String).GetString()!;
+
+    public readonly int GetInt32(string property) =>
+        GetPropertyAndValidate(property, JsonValueKind.Number).GetInt32();
+
+    public readonly MessageBody GetObject(string property) =>
+    new(GetPropertyAndValidate(property, JsonValueKind.Object));
+
+    public readonly JsonElement GetArray(string property) =>
+        GetPropertyAndValidate(property, JsonValueKind.Array);
+
+    private readonly JsonElement GetPropertyAndValidate(string propertyName, JsonValueKind expectedKind)
+    {
+        JsonElement? propertyElement = TryGetProperty(propertyName);
+
+        if (!propertyElement.HasValue)
+        {
+            throw new ArgumentException($"Property '{propertyName}' not found on message body object."); 
+        }
+
+        if (!ValidateProperty(propertyElement.Value, expectedKind))
+        {
+            throw new ArgumentException($"Property '{propertyName}' is not a valid {expectedKind}."); 
+        }
+
+        return propertyElement.Value;
+    }
+
+    private readonly JsonElement? TryGetProperty(string propertyName) =>
+     _json.ValueKind == JsonValueKind.Object && _json.TryGetProperty(propertyName, out var element)
+         ? element
+         : null;
+
+    private readonly JsonElement? TryGetProperty(string propertyName, JsonValueKind expectedKind)
+    {
+        var element = TryGetProperty(propertyName);
+        return (element.HasValue && ValidateProperty(element.Value, expectedKind))
+            ? element.Value
+            : null;
+    }
+
+    private readonly bool ValidateProperty(JsonElement element, JsonValueKind expectedKind) =>
+        element.ValueKind == expectedKind;
 
 
     private bool Validate(JsonValueKind kind) => _json.ValueKind == kind;
+
     private JsonElement ValidateJsonOrThrow(JsonValueKind valueKind)
     {
         if (!Validate(valueKind))
@@ -110,4 +183,3 @@ public readonly struct MessageBody
         return _json;
     }
 }
-
