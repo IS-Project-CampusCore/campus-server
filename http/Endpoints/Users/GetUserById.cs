a824aa6a26f91e754f12b1939c38354b56b3bdc3
Doru <doru.boca@cityscroller.com>
2026-01-18 21:09:06 +0200

    get user by id

---
+++ http/Endpoints/Users/GetUserById.cs
@@ -0,0 +1,33 @@
+﻿using commons.Protos;
+using FastEndpoints;
+using usersServiceClient;
+
+namespace http.Endpoints.Users;
+
+public class GetUserById(ILogger<GetAllUsers> logger) : CampusEndpoint<string>(logger)
+{
+    public usersService.usersServiceClient Client { get; set; } = default!;
+
+    public override void Configure()
+    {
+        Get("api/users/user");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(string req, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(req))
+        {
+            await HandleErrorsAsync(400, "User ID not found", cancellationToken);
+            return;
+        }
+
+        var grpcRequest = new UserIdRequest
+        {
+            Id = req
+        };
+
+        MessageResponse response = await Client.GetUserByIdAsync(grpcRequest, null, null, cancellationToken);
+        await SendAsync(response, cancellationToken: cancellationToken);
+    }
+}
